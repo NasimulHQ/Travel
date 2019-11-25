2a4850350dc34964f07624d07cecf7d086a9263f
NasimulHQ <nasimul345@gmail.com>
2019-11-26 00:47:11 +0600

    first commit

---
+++ bashbord.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>DashBord</title>
+    
+    <link href="css/main.css" rel="stylesheet">
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<link href="css/animate.css" rel="stylesheet">
+	<link href="css/jquery.bxslider.min.css" rel="stylesheet">
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/start/jquery-ui.css">
+	<link href="css/owl.carousel.css" rel="stylesheet">
+	<link href="css/owl.theme.default.css" rel="stylesheet">
+</head>
+<body>
+   
+   <div class="jum">
+       
+   </div>
+    
+</body>
+</html>
